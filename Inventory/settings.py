93ba804827b89dadb1940a8878620db7df2b2950
Ait0ne <bonafide112358@gmail.com>
2020-05-01 16:51:26 +0300

    Second

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd)8r5!7v-jyga!45qk8(#66*!=83c&_gkpl8nzqsu3c2til!u6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['134.122.65.218']
+ALLOWED_HOSTS = ['157.230.22.146']
 
 
 # Application definition
